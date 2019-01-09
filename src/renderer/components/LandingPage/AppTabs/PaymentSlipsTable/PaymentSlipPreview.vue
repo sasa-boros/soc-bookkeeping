87b0589c3a839c9d3cadf437ac7038767ead596e
popovicradmila <popovicradmila3@gmail.com>
2019-01-09 16:32:09 +0100

    form validation tooltips

--- src/renderer/components/LandingPage/AppTabs/PaymentSlipsTable/PaymentSlipPreview.vue
+++ src/renderer/components/LandingPage/AppTabs/PaymentSlipsTable/PaymentSlipPreview.vue
@@ -3,13 +3,13 @@
     <b-form @submit="onSubmit" @reset="onReset" v-if="show" no-validation>
       <div class="payment-slip-preview-text">
         <h1> УПЛАТНИЦА </h1>
-      <br/>На дин. <b-form-input required v-model="form.amount" class="input-small" id="amountInput" type="text" placeholder="______________________________________"></b-form-input> и словима <b-form-input required v-model="generatedAmountWithLetters" class="input-small" id="amountWithLettersInput" type="text" placeholder="_______________________________________________________________________________________"></b-form-input>
-      <br/><b-form-input class="input-small" id="amountWithLettersInputPt2"  type="text" placeholder="_________________________________________________________________________________________________________________________________________"></b-form-input>
-      <br/>колико сам данас уплатио у благајну Српске православне црквене општине<br/>у <b-form-input required v-model="form.town" class="input-small" id="townInput" type="text" placeholder="___________________________________________________________________________"></b-form-input> на име <b-form-input required v-model="form.reason" class="input-small" id="reasonInput" type="text" placeholder="_______________________________________________________________________________________"></b-form-input>
+      <br/>На дин. <b-form-group class="input-form-group"><b-form-input v-model="form.amount" v-b-tooltip.hover.html="missingAmountTooltipText" class="input-small" v-bind:class="{ 'is-invalid': attemptSubmit && missingAmount }" id="amountInput" type="number" min="0" step=".01" placeholder="______________________________________"></b-form-input></b-form-group> и словима <b-form-group class="input-form-group"><b-form-input v-model="generatedAmountWithLetters" class="input-small" id="amountWithLettersInput" type="text" placeholder="_______________________________________________________________________________________"></b-form-input></b-form-group>
+      <br/><b-form-group class="input-form-group"><b-form-input class="input-small" id="amountWithLettersInputPt2"  type="text" placeholder="_________________________________________________________________________________________________________________________________________"></b-form-input></b-form-group>
+      <br/>колико сам данас уплатио у благајну Српске православне црквене општине<br/>у <b-form-group class="input-form-group"><b-form-input v-model="form.town" class="input-small" v-b-tooltip.hover.html="missingTownTooltipText" v-bind:class="{ 'is-invalid': attemptSubmit && missingTown }" id="townInput" type="text" placeholder="___________________________________________________________________________"></b-form-input></b-form-group> на име <b-form-group class="input-form-group"><b-form-input v-model="form.reason" v-b-tooltip.hover.html="missingReasonTooltipText" class="input-small" v-bind:class="{ 'is-invalid': attemptSubmit && missingReason }" id="reasonInput" type="text" placeholder="_______________________________________________________________________________________"></b-form-input></b-form-group>
       <div class="mt-2">                                                                                                                                        У п л а т и о,
-                                                                                                            <b-form-input class="input-small" id="payerInput" type="text" placeholder="_______________________________________________________________________________"></b-form-input>   
+                                                                                                            <b-form-group class="input-form-group"><b-form-input class="input-small" id="payerInput" type="text" placeholder="_______________________________________________________________________________"></b-form-input></b-form-group>  
       </div><div class="mt-2">                                                                                                          Књижити у корист буџета за                                                                        
-      <br/><b-form-input class="input-small" type="text" placeholder="................................................................"></b-form-input>                                                    
+      <br/><b-form-group class="input-form-group"><b-form-input class="input-small" type="text" placeholder="................................................................"></b-form-input></b-form-group>                                                   
       </div>
       
       Примио благајник, 
@@ -34,30 +34,42 @@ export default {
         town: null,
         amountWithLetters: null
       },
+      attemptSubmit: false,
       show: true
     }
   },
   methods: {
     onSubmit (evt) {
       evt.preventDefault()
-      paymentSlipsController.createPaymentSlip(this.form)
-      this.$root.$emit('bv::refresh::table', 'payment-slips-table')
-      this.form.amount = null
-      this.form.reason = null
-      this.form.town = null
-      this.form.amountWithLetters = null
-      this.$root.$emit('bv::hide::modal', 'modalCreateSlip')
+      this.attemptSubmit = true
+      if (this.checkForm()) {
+        paymentSlipsController.createPaymentSlip(this.form)
+        this.$root.$emit('bv::refresh::table', 'payment-slips-table')
+        this.resetForm()
+        this.$root.$emit('bv::hide::modal', 'modalCreateSlip')
+      }
     },
     onReset (evt) {
       evt.preventDefault()
-      /* Reset our form values */
+      this.resetForm()
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false
+      this.$nextTick(() => { this.show = true })
+    },
+    checkForm () {
+      if (!this.form.amount ||
+          !this.form.reason ||
+          !this.form.town) {
+        return false
+      }
+      return true
+    },
+    resetForm () {
       this.form.amount = null
       this.form.reason = null
       this.form.town = null
       this.form.amountWithLetters = null
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false
-      this.$nextTick(() => { this.show = true })
+      this.attemptSubmit = false
     }
   },
   computed: {
@@ -66,6 +78,36 @@ export default {
         return numberToSerbianDinars(this.form.amount)
       },
       set: function (newValue) {
+      }
+    },
+    missingReason: function () {
+      return !this.form.reason || this.form.reason.trim() === ''
+    },
+    missingTown: function () {
+      return !this.form.town || this.form.town.trim() === ''
+    },
+    missingAmount: function () {
+      return !this.form.amount || this.form.amount.trim() === ''
+    },
+    missingReasonTooltipText: function () {
+      if (this.missingReason && this.attemptSubmit) {
+        return 'Унесите вредност'
+      } else {
+        return ''
+      }
+    },
+    missingTownTooltipText: function () {
+      if (this.missingTown && this.attemptSubmit) {
+        return 'Унесите вредност'
+      } else {
+        return ''
+      }
+    },
+    missingAmountTooltipText: function () {
+      if (this.missingAmount && this.attemptSubmit) {
+        return 'Унесите вредност'
+      } else {
+        return ''
       }
     }
   }
@@ -78,7 +120,6 @@ export default {
   height: 559px; 
   border-style: solid;
   border-width: thin;
-  font-family: "Times New Roman";
   position: relative;
 }
 .payment-slip-preview-text{
@@ -106,17 +147,27 @@ h1{
   margin: 0px;
   padding: 0px;
 }
+.input-form-group{ 
+  display: inline;
+  height: 15px;
+  margin: 0px;
+  padding: 0px;
+}
 .input-small{
   border-style: none;
   font-weight: bold;
-  padding: 2px;
   display: inline;
   height: 15px;
+  margin: 0px;
+  padding: 0px;
 }
 .input-small::placeholder{
   border-style: none;
   font-weight: normal;
   color: #d3d3d3;
+}
+.is-invalid {
+  border-style: dotted;
 }
 #amountInput{
   width: 140px;
